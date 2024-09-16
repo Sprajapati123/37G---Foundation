c1a9a35958ad1c49ffcae2c8c10511d86d17ebff
sprajapati123 <sandisprajapati52@gmail.com>
2024-09-16 15:35:23 +0545

    swithc

---
+++ switch.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main(){
+    char alphabet;
+    printf("Enter any alphabet: ");
+    scanf("%c",&alphabet);
+    
+    switch(alphabet){
+        case 'a':
+        printf("a is vowel\n");
+        break;
+        case 'e':
+        printf("e is vowel\n");
+        break;
+        case 'i':
+        printf("i is vowel\n");
+        break;
+        case 'o':
+        printf("o is vowel\n");
+        break;
+        case 'u':
+        printf("u is vowel\n");
+        break;
+        default:
+        printf("Not a  vowel\n");
+        break;
+
+        // Enter days of week
+        /*
+        1 - sunday
+        2-monday
+        3-tuesday
+        default: Invalid input
+        */
+    }
+    // if(condition){
+        
+    // }else if(condition){
+        
+    // }else{
+        
+    // }
+}

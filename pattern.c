4b2ca8803fe1dc2ea8a05dfd9feaea3755cc514e
sprajapati123 <sandisprajapati52@gmail.com>
2024-09-18 15:48:25 +0545

    pattern

---
+++ pattern.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    for(int i = 1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}

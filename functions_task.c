edf780799487cad05b57ad2ddd34ed4f23db0756
sprajapati123 <sandisprajapati52@gmail.com>
2024-09-20 16:28:15 +0545

    function task

--- functions_task.c
+++ functions_task.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
 
+//function declaration
+int add(int,int);
+void subtract(int,int);
+int multiply(int,int);
+
 int main(){
     int first,second,choice;
+    int i = 1;
+    while(i){
+        printf("\nChoose option (1-3)\n");
+    printf("1. Add\n2. Subtract\n3. Multiply\n");
+    scanf("%d",&choice);
+
+    if(choice <=3 && choice > 0){
+        printf("Enter first number: ");
+        scanf("%d",&first);
+
+        printf("Enter second number: ");
+        scanf("%d",&second);
+        
+        switch(choice){
+            case 1:
+                int sum = add(first,second);
+                printf("The sum is %d",sum);
+                break;
+            case 2:
+                subtract(first,second);
+                break;
+            case 3:
+                int result = multiply(first,second);
+                printf("The result is %d",result);
+                break;
+            default:
+                break;
+        }
+    }else{
+        printf("Invalid input,\n");
+    }
+    }
+}
+
+//function defination
+int add(int a,int b){
+    return a+b;
+}
+
+void subtract(int a,int b){
+    printf("The difference is %d", a-b);
+}
+
+int multiply(int a, int b){
+    return a*b;
 }
